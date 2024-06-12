54d8433116eae29556723d4205cd7bd977cc2cbd
GRADI <56418645+gradikay@users.noreply.github.com>
2024-06-12 15:34:10 -0400

    Create QueueInterface.java

---
+++ QueueInterface.java
@@ -0,0 +1,12 @@
+package queue;
+
+public interface QueueInterface<T> {
+	public void enqueue(T newEntry);
+	public T dequeue();
+	public T getFront();
+	public boolean isEmpty();
+	public void clear();
+	public void splice(QueueInterface<T> anotherQueue);
+	public T getBack();
+
+}
